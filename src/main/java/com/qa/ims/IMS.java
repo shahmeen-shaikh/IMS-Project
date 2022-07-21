679621068af18da7d297c41d8044d90cd4921d29
shahmeen-shaikh <shahmeen_shaikh@outlook.com>
2022-07-21 20:33:12 +0100

    changes in ims

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -10,6 +10,8 @@ import com.qa.ims.controller.ItemController;
 import com.qa.ims.persistence.dao.CustomerDAO;
 import com.qa.ims.persistence.dao.ItemDAO;
 import com.qa.ims.persistence.domain.Domain;
+import com.qa.ims.controller.OrderController;
+import com.qa.ims.persistence.dao.OrderDAO;
 import com.qa.ims.utils.DBUtils;
 import com.qa.ims.utils.Utils;
 
@@ -19,15 +21,24 @@ public class IMS {
 
 	private final CustomerController customers;
 	private final ItemController items;
+	private final OrderController orders;
 	private final Utils utils;
 
 	public IMS() {
 		this.utils = new Utils();
-		final CustomerDAO custDAO = new CustomerDAO();
-		final ItemDAO itemDAO = new ItemDAO();
-		this.customers = new CustomerController(custDAO, utils);
-		this.items = new ItemController(itemDAO, utils);
-	}
+
+        final CustomerDAO custDAO = new CustomerDAO();
+
+        final ItemDAO ItemDAO = new ItemDAO();
+
+        final OrderDAO orderDAO = new OrderDAO(custDAO, ItemDAO);
+
+        this.customers = new CustomerController(custDAO, utils);
+
+        this.items = new ItemController(ItemDAO, utils);
+
+        this.orders = new OrderController(orderDAO, utils);
+			}
 
 	public void imsSystem() {
 		LOGGER.info("Welcome to the Inventory Management System!");
@@ -58,6 +69,7 @@ public class IMS {
 				active = this.items;
 				break;
 			case ORDER:
+				active = this.orders;
 				break;
 			case STOP:
 				return;
